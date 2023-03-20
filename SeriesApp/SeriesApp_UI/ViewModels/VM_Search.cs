2816c4439e7ffd4f60da760601730d6732d97b2f
Nadine <nadine.ru.ris@gmail.com>
2023-03-20 21:19:40 +0100

    ¡Búsqueda implementada!

--- SeriesApp/SeriesApp_UI/ViewModels/VM_Search.cs
+++ SeriesApp/SeriesApp_UI/ViewModels/VM_Search.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace SeriesApp_UI.ViewModels
 {
@@ -12,14 +13,44 @@ namespace SeriesApp_UI.ViewModels
 
         [ObservableProperty]
         string searchText;
+        
+        /// <summary>
+        /// Este método se ejecutará cuando la cadena SearchText cambie. Si esta está vacía o es nula se actualizará la lista de series para contenerlas todas.
+        /// 
+        /// ACLARACIÓN: Esto he tenido que hacerlo porque el Search Bar sólo llama al commando enlazado cuando la búsqueda contiene texto,
+        /// cuando está vacía o únicamente contiene espacios en blanco no se actualizaría.
+        /// </summary>
+        /// <param name="value"></param>
+        partial void OnSearchTextChanged(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                SearchResult = AllSeriesList;
+            }
+        }
 
         [ObservableProperty]
         List<ClsSeries> searchResult;
 
+        List<ClsSeries> AllSeriesList;
+
         public VM_Search()
         {
             seriesDAO = new SeriesDAO();
-            searchResult = seriesDAO.GetAll();
+            AllSeriesList = seriesDAO.GetAll();
+            SearchResult = AllSeriesList;
+        }
+
+        /// <summary>
+        /// Este método es ejecutado cuando el usuario pulsa en el botón de búsqueda y el texto introducido no es nulo, 
+        /// ni está vacío, ni contiene unicamente espacios en blanco. 
+        /// 
+        /// Se actualizará la lista de series SearchResult con las series que contengan en su nombre el texto de la búsqueda.
+        /// </summary>
+        [RelayCommand]
+        public async void SearchSerie()
+        {
+            SearchResult = AllSeriesList.Where(x => x.Name.Contains(SearchText, StringComparison.OrdinalIgnoreCase)).ToList();
         }
 
         /// <summary>
